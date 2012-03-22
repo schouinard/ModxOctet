6b101529bc4b0aa0bd9453fd103bc8f51639e5c5
Stephane Chouinard <schouinard@octetweb.ca>
2012-03-22 09:54:45 -0400

    Mise à jour du projet

--- js/behavior.js
+++ js/behavior.js
@@ -26,7 +26,96 @@ $(document).ready(function () {
     });
 
     $("#form-contact").validate();
+    
+    
+    $(".services div.img-service").click(function(){
+       if($(".services div.desc-service").filter(":visible").length)
+       {
+            if(!$(this).next().hasClass('open'))
+            {
+                $(".services div.desc-service").filter(":visible").fadeOut('slow');
+                $(".services div.desc-service").filter(":visible").removeClass('open');
+                $(this).next().fadeIn('slow');
+                $(this).next().addClass('open');
+            }
 
+       }
+       else
+       {
+           $(this).next().slideToggle(); 
+           $(this).next().toggleClass('open');
+       } 
+    });
+    
+    $(".realisations div.vignette").click(function(e){
+        e.preventDefault();
+        var me = $(this);
+        var divScreenshot = $('#realisation-screenshot'); 
+        if(me.hasClass('open'))
+        {
+            me.removeClass('open');
+            divScreenshot.slideUp('slow').removeClass('open');
+            
+        }
+        else
+        {
+            var rangee = me.parent().data('row');
+            var lien = me.children('a').attr('href');
+            $('.vignette.open').removeClass('open');
+            me.addClass('open');
+            
+           
+           var prochaineRangee = rangee + 1;
+           var premierItemProchaineRangee = $('div[data-row="' + prochaineRangee +'"]').first();
+           
+           divScreenshot.height(divScreenshot.width());
+           if(divScreenshot.prev().data('row') == rangee && divScreenshot.hasClass('open'))
+           {
+                divScreenshot.height(divScreenshot.height()).children().fadeOut('fast', function(){
+                   divScreenshot.load(lien, function(){
+                       divScreenshot.children().hide();
+                       divScreenshot.children().fadeIn('fast', function(){});
+                   });
+               });
+               
+               
+           }
+           else{
+               divScreenshot.slideUp('slow', function(){
+                   divScreenshot.load(lien, function(){
+                       if(premierItemProchaineRangee.length)
+                       {
+                           $(premierItemProchaineRangee).before(divScreenshot);
+                       }
+                       else{
+                           $('div[data-row="' + rangee + '"]').last().after(divScreenshot);
+                       }               
+                       divScreenshot.slideDown();
+                       divScreenshot.addClass('open');
+                        });
+               });
+           } 
+       }
+       
+       
+       /* if($(".realisations div.image").filter(":visible").length)
+       {
+            if(!$(this).next().hasClass('open'))
+            {
+                $(".realisations div.image").filter(":visible").fadeOut('slow');
+                $(".realisations div.image").filter(":visible").removeClass('open');
+                $(this).next().fadeIn('slow');
+                $(this).next().addClass('open');
+            }
+
+       }
+       else
+       {
+           $(this).next().slideToggle(); 
+           $(this).next().toggleClass('open');
+       }    */
+       
+    });
 });
 
 
